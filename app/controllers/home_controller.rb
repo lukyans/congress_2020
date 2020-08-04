f60606acceb53d637e89b5e137a78499feb27fc9
Sergey <sukyans@gmail.com>
2020-08-04 15:00:23 -0400

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
   def index
-    @members = Member.all
+    @members = Member.where(state: params[:state])
     @current_state_members = params[:current_state_members]
+  end
+
+  def sort_asc
+    state = params[:state]
+    @members = Member.where(state: state).order(name: :asc)
+  end
+
+  def sort_desc
+    state = params[:state]
+    @members = Member.where(state: state).order(name: :desc)
   end
 end
