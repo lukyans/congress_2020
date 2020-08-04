0d0eaf73e0ef7aa74a627b44ab6ebeba89a314fb
Sergey <sukyans@gmail.com>
2020-08-04 15:00:59 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index", as: :root
   get "/search", to: "search#index"
-  get 'search/sort_asc' => 'search#sort_asc'
-  get 'search/sort_desc' => 'search#sort_desc'
+  get 'home/sort_asc' => 'home#sort_asc'
+  get 'home/sort_desc' => 'home#sort_desc'
 end
