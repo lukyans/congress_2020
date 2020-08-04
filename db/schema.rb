a2424aec7b2972d7bad83c974d4eb9dc528db4ac
Sergey <sukyans@gmail.com>
2020-08-04 15:01:05 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_235756) do
+ActiveRecord::Schema.define(version: 2020_08_04_181239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_235756) do
     t.string "api_uri"
     t.string "district"
     t.string "at_large"
+    t.string "state"
   end
 
 end
